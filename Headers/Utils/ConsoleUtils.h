45b655829e08f2b9c2e902e2c8f3834df126516d
Caleb B <brodock1234@gmail.com>
2023-03-07 22:58:24 -0800

    Hopefully Fixed

--- Headers/Utils/ConsoleUtils.h
+++ Headers/Utils/ConsoleUtils.h
@@ -8,9 +8,9 @@ using namespace std;
 
 
 namespace ConsoleUtils {
-	void printLn(string msg) { cout << msg << endl; }
-	void printLn(char msg) { cout << msg << endl; }
-	void printLn(int msg) { cout << msg << endl; }
+	void printLn(string msg);
+	void printLn(char msg);
+	void printLn(int msg);
 
 	void error(string out);
 	void error(char out);
--- lib/ConsoleUtils.cpp
+++ lib/ConsoleUtils.cpp
@@ -1,6 +1,18 @@
 #include "Headers/Utils/ConsoleUtils.h"
 
 
+void ConsoleUtils::printLn(string msg) {
+	cout << msg << endl;
+}
+
+void ConsoleUtils::printLn(char msg) {
+	cout << msg << endl;
+}
+
+void ConsoleUtils::printLn(int msg) {
+	cout << to_string(msg) << endl;
+}
+
 void ConsoleUtils::error(string out) {
 	cerr << out;
 }
