a5b1b9953ce4e2571d11b1c7bab48d89285fe326
Caleb B <brodock1234@gmail.com>
2023-03-07 20:48:01 -0800

    printLn() minor expansion

--- Utils/Headers/ConsoleUtils.h
+++ Utils/Headers/ConsoleUtils.h
@@ -9,6 +9,8 @@ using namespace std;
 
 namespace ConsoleUtils {
 	void printLn(string msg) { cout << msg << endl; }
+	void printLn(char msg) { cout << msg << endl; }
+	void printLn(int msg) { cout << msg << endl; }
 
 	void error(string out);
 	void error(char out);
